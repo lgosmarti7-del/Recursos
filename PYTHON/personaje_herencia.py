a9eca4451fa9e9ef704b2789217d670da6895deb
Martin <lgosmarti7@gmail.com>
2026-04-22 09:49:27 -0400

    Update personaje_herencia.py

--- PYTHON/personaje_herencia.py
+++ PYTHON/personaje_herencia.py
@@ -1,1 +1,31 @@
+class Personaje:
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+        self.nombre = nombre
+        self.fuerza = fuerza
+        self.inteligencia = inteligencia
+        self.defensa = defensa
+        self.vida = vida
 
+    def atributos(self):
+        print(self.nombre, ":", sep="")
+        print("·Fuerza:", self.fuerza)
+        print("·Inteligencia:", self.inteligencia)
+        print("·Defensa:", self.defensa)
+        print("·Vida:", self.vida)
+
+# Aquí empieza la Herencia
+class Guerrero(Personaje):
+    # La clase Guerrero hereda de Personaje
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, espada):
+        # Usamos super() para no repetir el código del padre
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        self.espada = espada # Atributo nuevo solo del guerrero
+
+    # Podemos cambiar un método (Sobrecarga)
+    def atributos(self):
+        super().atributos()
+        print("·Espada:", self.espada)
+
+# Prueba básica
+mi_guerrero = Guerrero("Aragorn", 20, 10, 10, 100, 5)
+mi_guerrero.atributos()
