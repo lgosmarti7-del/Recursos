445039bcbe3e3cb87a2469e17f4257901958dbd0
Martin <lgosmarti7@gmail.com>
2026-05-06 09:57:43 -0400

    Update Lenguaje.java

--- Java/Lenguaje.java
+++ Java/Lenguaje.java
@@ -1,1 +1,5 @@
-
+class Main {
+    public static void main(String[] args) { // Declaramos variables de tipo texto
+        String libro = "El programador pragmático"; // Declaramos una variable de tipo String para almacenar texto
+    }
+}
