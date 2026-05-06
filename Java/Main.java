b3da9d487255aa7b477c9f092567e9345a32e800
Martin <lgosmarti7@gmail.com>
2026-05-06 10:13:16 -0400

    Update Main.java

--- Java/Main.java
+++ Java/Main.java
@@ -1,1 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // E4.04: Hola Mundo en Java
+        System.out.println("Hola mundo");
 
+        // E4.07: Ejemplo de uso de la clase Lenguaje
+        Lenguaje java = new Lenguaje("Java", 1995);
+        java.descripcion();
+    }
+}
